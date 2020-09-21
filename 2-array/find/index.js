25984e9cbda7a8bdc59d00a823927cac50bf498f
g992987642 <992987642@qq.com>
2020-09-21 20:03:53 +0800

    [guhao]:finished exercise 2 completely

--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,3 +1,6 @@
 export default function find00OldPerson(collection) {
+  let result = collection.find(e => e.age > 10 && e.age <= 20);
+
+  return result.name;
   // TODO 4: 在这里写实现代码
 }
--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,5 @@
 export default function numberMapToWord(collection) {
   // TODO 3: 在这里写实现代码
+  let retult = collection.map(e => String.fromCharCode(96 + e));
+  return retult;
 }
