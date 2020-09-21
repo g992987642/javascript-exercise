c93c33c7da0a11cdf6ad4a375378f10140a66fb2
g992987642 <992987642@qq.com>
2020-09-21 20:22:08 +0800

    [guhao]:finished exercise 3

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,12 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  const temp = {
+    serialNumber: '12345',
+    properties: {
+      color: 'green',
+      status: 'processed'
+    }
+  };
+  let result = Object.assign({}, source, temp);
+  return result;
 }
--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,6 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  let result = 0;
+  Object.values(source).forEach(e => (result = result + Number.parseInt(e)));
+  return result;
 }
